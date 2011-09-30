f6e7ff7ac0e2bd6158ec33433a7588259f38bad2
Sven Wagner <sven.wagner@sinnerschrader.com>
2011-09-30 13:24:10 +0200

    first commit

---
+++ src/contentScript.js
@@ -0,0 +1,36 @@
+/*
+ 
+||Wo|| ||
+|Instanz (Live, Stage, Devel)|devel | 
+|Browser| im IE 6 geprüft|
+|Betriebssytem (Win XP, Win 7,  Mac OS X) | Win XP|
+|Sprache|de | 
+|Screenshot/Screencast| ja |
+|Skin (Consumer, Member, Agentur)|agent  | 
+|Nutzername + Passwort|  -- | 
+|Buchungsnummern (PNRs)| -- |
+
+h2. *Wie (Reproduzierungsschritte)*
+Schritt-Für-Schritt-Beschreibung (1. 2. 3...), "von www.TUIfly.com bis zum Fehlerpunkt"
+# BookingList im IE 6 aufrufen
+
+{color:red}*Eigentliche Fehlerbeschreibung (Wie äußert sich der Fehler?)*{color}
+# Browser Update Leiste wird doppelt agezeigt
+
+{color:green}*Beschreibung des eigentlich erwarteten Verhaltens (Was soll geschehen?)*{color}
+# Update Leiste nur einmal anzeigen
+ 
+ */
+
+var $desc = $("#descriptionFieldArea");
+if($desc.length){
+	var $helpIcon = $desc.find("#viewHelp");
+	$helpIcon.after("<span id='jm-init'>JM</span>");
+	
+	$("#jm-init").live("click", function(){
+		var $area = $desc.find("textarea:first");
+		var $parent = $area.parent();
+		$area.hide();		
+		$parent.append("test");
+	})
+}
